f081d53572bb59152ac7786381ca5a295e9f487c
duonvipbn <duonvipbn@gmail.com>
2024-12-06 16:39:00 +0700

    Update HN_JPN_KS24B_NguyenTungDuong_01.c

--- HN_JPN_KS24B_NguyenTungDuong_01.c
+++ HN_JPN_KS24B_NguyenTungDuong_01.c
@@ -1,121 +1,153 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int main(){
-	int n, cols, tongSoHoanHao, tongCacSoHoanHao, max, maxSec, newElement, newIndex;
-	int arr[100];
+	int n, rows, cols;
+	int arr[100][100];
 	do{
-		printf("MENU\n");
-		printf("1. Nhap so phan tu va gia tri cho mang\n");
-		printf("2. In ra gia tri cac phan tu trong mang\n");
-		printf("3. Dem so luong cac so hoan hao co trong mang\n");
-		printf("4. Tim gia tri nho thu 2 trong mang\n");
-		printf("5. Them mot phan tu vao vi tri dau tien trong mang\n");
-		printf("6. Xoa phan tu tai mot vi tri cu the\n");
-		printf("7. Sap xep mang theo thu tu giam dan (Selection sort)\n");
-		printf("8. Tim kiem phan tu trong mang, phan tu tim kiem do nguoi dung nhap vao. Su dung thuat toan Binary search\n");
-		
-		printf("11. Thoat\n");
-		printf("Chon: ");
+		printf("---MENU---\n");
+		printf("1. Nhap kich co va gia tri cac phan tu cua mang\n");
+		printf("2. In gia tri cac phan tu cua mang theo ma tran\n");
+		printf("3. In ra cac phan tu nam tren duong bien va tinh tich\n");
+		printf("4. In ra cac phan tu nam tren duong cheo chinh\n");
+		printf("5. In ra cac phan tu nam tren duong cheo phu\n");
+		printf("6. Sap xep duong cheo chinh theo thu tu tang dan\n");
+		printf("7. Tim kiem mot phan tu va in ra vi tri cua phan tu do trong mang\n");
+		printf("8. Thoat khoi chuong trinh\n");
+		printf("Moi ban nhap lua chon: ");
 		scanf("%d", &n);
 		
 		switch(n){
+			
 			case 1:
-				printf("Nhap so phan tu muon them vao mang trong khoan [0; 100]: ");
+				printf("nhap so cot[0; 100]: ");
+				scanf("%d", &rows);
+				
+				printf("nhap so hang[0; 100]: ");
 				scanf("%d", &cols);
-				if(cols >= 0 && cols <= 100){
-					for(int i = 0; i < cols; i++){
-						printf("arr[%d]=", i);
-						scanf("%d", &arr[i]);
+				
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100){
+					for(int i = 0; i < rows; i++){
+						for(int j = 0; j < cols; j++){
+							printf("arr[%d][%d]=", i, j);
+							scanf("%d", &arr[i][j]);	
+						}
 					}
+					
 				}else{
-					printf("So phan tu them vao mang sai\n");	
+					printf("nhap lai so cot so hang\n");
 				}
 				break;
 			case 2:
-				for(int i = 0; i < cols; i++){
-						printf("arr[%d] = %d, ", i, arr[i]);
-				}
-				printf("\n");
-				break;
-			case 3:
-				tongSoHoanHao = 0;
-				tongCacSoHoanHao = 0;
-				for(int i = 0; i < cols; i++){
-					for(int j = 1; j < arr[i]; j++){
-						if(arr[i] % j == 0){
-							tongSoHoanHao += j;
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100){
+					for(int i = 0; i < rows; i++){
+						for(int j = 0; j < cols; j++){
+							printf("%d ", arr[i][j]);	
 						}
+						printf("\n");
 					}
-					if(tongSoHoanHao != 0 && tongSoHoanHao == arr[i]){
-						tongCacSoHoanHao++;
-					}
-				}
-				printf("So luong cac so hoan hao co trong mang la: %d\n", tongCacSoHoanHao);
-				break;	
-			case 4:
-				max = 0;
-				maxSec = 0;
-				for(int i = 0; i < cols; i++){
-					if(max < arr[i]){
-						max = arr[i];
-					}
-				}
-				for(int i = 0; i < cols; i++){
-					if(arr[i] != max){
-						if(maxSec < arr[i]){
-							maxSec = arr[i];							
-						}
-					}
-				}
-				printf("Gia tri nho thu 2 trong mang la: %d\n", maxSec);
-				break;
-			case 5:
-				printf("nhap so phan tu muon them: ");
-				scanf("%d", &newElement);
-				cols++;
-				for(int i = cols-1; i >= 0; i--){
-					arr[i+1] = arr[i];
-				}
-				arr[0] = newElement;
-				
-				break;	
-			case 6:
-				printf("Nhap vi tri muon xoa: ");
-				scanf("%d", &newIndex);
-				if(newIndex >= cols){
 					
 				}else{
-					cols --;
-					for(int i = newIndex; i < cols; i++){
-						arr[i] = arr[i+1];
-					}	
+					printf("nhap lai so cot so hang\n");
 				}
 				break;
+			case 3:
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100){
+					int tong = 0;
+
+					for(int i = 0; i < cols; i++){
+						tong += arr[0][i];
+						printf("%d ",arr[0][i]);	
+					}
+
+					for(int i = 0; i < cols; i++){
+						tong += arr[rows-1][i];
+						printf("%d ",arr[rows-1][i]);	
+					}
+
+					for(int i = 1; i < rows-1; i++){
+						tong += arr[i][0];
+						printf("%d ",arr[i][0]);	
+					}
+
+					for(int i = 1; i < rows-1; i++){
+						tong += arr[i][cols-1];
+						printf("%d ",arr[i][cols-1]);	
+					}
+					printf("\nTong: %d\n", tong);
+					
+				}else{
+					printf("nhap lai so cot so hang\n");
+				}
+				break;
+			case 4:
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100 && cols == rows){
+					for(int i = 0; i < rows; i++){
+						printf("%d ", arr[i][i]);
+					}
+					printf("\n");
+				}else{
+					printf("nhap lai so cot so hang(so cot = so hang)\n");
+				}	
 				
-			case 7:
-				for (int i = 0; i < cols - 1; i++){
-					int min = i;
-					for(int j = i + 1; j < cols; j++){
-						if(arr[j] > arr[min]){
-							min = j;
+				break;
+			case 5:
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100 && cols == rows){
+					for(int i = 0; i < rows; i++){
+						printf("%d ", arr[i][rows-1-i]);
+					}
+					printf("\n");
+				}else{
+					printf("nhap lai so cot so hang(so cot = so hang)\n");
+				}	
+				
+				break;
+			case 6:
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100 && cols == rows){
+					for(int i = 0; i < rows-1; i++){
+						for(int j = 0; j < rows-1-i; j++){
+							if(arr[j][j]>arr[j+1][j+1]){
+								int temp = arr[j][j];
+								arr[j][j] = arr[j+1][j+1];
+								arr[j+1][j+1] = temp;
+							}
 						}
 					}
-					int temp = arr[min];
-					arr[min] = arr[i];
-					arr[i] = temp;
-				}
-				break;
-			case 8:
-			
-				break;
-				
-			case 11:
-				printf("Thoat");
-				break;							
-			default:
+					printf("da doi vi chi phan tu tren duong treo theo thu tu tang dan\n");
+				}else{
+					printf("nhap lai so cot so hang(so cot = so hang)\n");
+				}	
 				
 				break;	
-		}
-	}while(n != 11);
-	return 0;
+			case 7:
+				if(rows > 0 && cols > 0 && rows < 100 && cols < 100){
+					int nhap;
+					printf("nhap mot phan tu muon tim: ");
+					scanf("%d", &nhap);
+					
+					for(int i = 0; i < rows; i++){
+						for(int j = 0; j < cols; j++){
+							if(arr[i][j]==nhap){
+								printf("vi tri phan tu %d nam trong arr[%d][%d]", arr[i][j], i, j);
+								break;
+							}	
+						}
+					}
+				}else{
+					printf("nhap lai so cot so hang\n");
+				}	
+				break;	
+			case 8:
+				printf("thoat\n");
+					
+				break;	
+			default:
+			
+				break;
+					
+			
+		}	
+	}while(n!=8);	
+	
+ 		
+ 	return 0;
 }
